ce09ac90a448482eb3a22233fea641ca7773a556
Tejeswar Sahu <tsahu@aipl-lt-1199.local>
2020-01-09 12:49:51 +0530

    this code is configured with 3306 mysql port

---
+++ 04-todo-web-application-mysql/src/main/java/com/in28minutes/springboot/web/SpringBootFirstWebApplication.java
@@ -0,0 +1,29 @@
+package com.in28minutes.springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.in28minutes.springboot.web")
+public class SpringBootFirstWebApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootFirstWebApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstWebApplication.class, args);
+		System.out.println("=================================================================");
+		String igniteEnvRestValue = System.getenv("");
+		System.out.println("igniteEnvRestValue"+igniteEnvRestValue);
+		
+		
+		System.out.println("=================================================================");
+		System.out.println("End of main method....");
+	}
+
+}
